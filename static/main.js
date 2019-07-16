250bfc289e6bcc2cf7a688e8ba715b40e1e34b1b
Nikita Furin <nokados@yandex.ru>
2019-07-16 11:53:44 +0000

    Buttons animation

--- static/main.js
+++ static/main.js
@@ -1,25 +1,10 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
-
-    $( ".entry" ).each(function( index ) {
-      console.log( index + ": " + $( this ).text() );
-    });
-    
-    $('.entry').on('click', function() {
-      console.log('click')
-      var entry = this;
-      var post_id = $(this).find('h2').attr('id');
-      $.ajax({
-        type:'GET',
-        url: '/delete' + '/' + post_id,
-        context: entry,
-        success:function(result) {
-          if(result.status === 1) {
-            $(this).remove();
-            console.log(result);
-          }
-        }
-      });
-    });
-
+    $(".btn76").click(function() {
+        console.log('click');
+        $(this).addClass('animated fadeOutUp faster');
+        $(this).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            console.log('endanimation');
+            $(this).removeClass('animated fadeOutUp faster');
+        });
+    })
 });
