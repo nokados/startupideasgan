2b6a8ba1e7d075ea5f7a669e33324c2c50e25d14
Nikita Furin <nokados@yandex,ru>
2019-07-17 15:26:54 +0000

    Loading animation if client spent all ideas

--- static/main.js
+++ static/main.js
@@ -12,6 +12,9 @@ $( document ).ready(function() {
         $(this).removeClass();
     });
 
+    var num_of_ideas = 5;
+    var cur = ideas.shift()
+
     function set_idea(idea) {
         var header = $('#cover-heading')
         header.text(idea);
@@ -24,16 +27,16 @@ $( document ).ready(function() {
     var is_runned = false;
     function get_ideas() {
         if (is_runned) {
-            console.warn('Already runned.')
+            console.log('Already runned.')
             setTimeout(function() {
                 console.error('timeout error. running is allowed again');
                 is_runned = false;
-            }, 2);
+            }, 500);
             return
         }
         is_runned = true;
         $.post('/get_ideas').done(function(response) {
-                console.warn('Ideas has gotten!')
+                console.log('Ideas has gotten!')
                 var new_ideas = response['ideas'];
                 ideas.push(...new_ideas)
                 is_runned = false;
@@ -43,17 +46,36 @@ $( document ).ready(function() {
             });
     }
     
-    var num_of_ideas = 5;
-    var cur = ideas.shift()
-    $('#skip').click(function() {
+    function loading_loop() {
+        console.log('Loading...')
+        get_ideas()
         if (ideas.length < num_of_ideas) {
-            console.warn('Getting new ideas...')
+            setTimeout(loading_loop, 300)
+        } else {
+            $('#loading').hide()
+            $('#cover-heading').show()
+            show_next_idea()
+        }
+    }
+
+    function loading_ideas() {
+        $('#cover-heading').hide()
+        $('#loading').show()
+        setTimeout(loading_loop(), 1)
+    }
+    
+    function show_next_idea() {
+        if (ideas.length < num_of_ideas) {
+            console.log('Getting new ideas...')
             get_ideas()
         }
         if (ideas.length === 0) {
+            loading_ideas()
             return;
         }
         cur = ideas.shift()
         set_idea(cur); // ideas are firstly initialized in index.html 
-    })
+    }
+    
+    $('#skip').click(show_next_idea)
 });
